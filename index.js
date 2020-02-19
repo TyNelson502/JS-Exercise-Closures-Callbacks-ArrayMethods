86e4c454e52e4521e77b7f51bc5b3c211fcae887
Tyler Nelson <tyler.d.nelson@outlook.com>
2020-02-18 21:20:44 -0500

    finished project

--- index.js
+++ index.js
@@ -48,8 +48,9 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
+function processLength(list, callback) {
   /* CODE HERE */
+  return callback(list.length);
 }
 
 /**
@@ -66,8 +67,10 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
+function processLastItem(stringList, callback) {
   /* CODE HERE */
+  return callback(stringList[stringList.length - 1]);
+
 }
 
 /**
@@ -87,9 +90,11 @@ function processLastItem(/* CODE HERE */) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
+function processSum(numberList, callback) {
   /* CODE HERE */
-}
+ return callback(numberList.reduce((total, numberList)) function getSum {
+   return total += numberList;}, 0))
+ }
 
 /**
  * ### Challenge `processProduct`
@@ -109,8 +114,8 @@ function processSum(/* CODE HERE */) {
  * [2] Invoking `processProduct` passing 25 and 0 and `(num) => num + 1000`,
  * should return 1000.
 */
-function processProduct(/* CODE HERE */) {
-  /* CODE HERE */
+function processProduct(num1, num2, callback) {
+  return callback(num1 * num2);
 }
 
 /**
@@ -133,8 +138,13 @@ function processProduct(/* CODE HERE */) {
  * "lady gaga" and `['foo', 'bar']` and `(bool) => bool ? 'nice!' : 'sad'`,
  * should return "sad".
 */
-function processContains(/* CODE HERE */) {
+function processContains(item, list, callback) {
   /* CODE HERE */
+  let filteredArray = list.filter((value) => value === item);
+  let bool = (filteredArray[0] === item);
+  return callback(bool);
+}
+return callback()
 }
 
 /**
@@ -194,8 +204,10 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
+function firstNamesAllCaps(runners) {
   /* CODE HERE */
+  
+  return runners.map((runners) => `${runners.first_name.toUpperCase()}`)
 }
 
 /**
@@ -211,8 +223,9 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  const runnersOfSize = runners.filter((value) => value.shirt_size === tShirtSize);
+  return runnersOfSize;
 }
 
 /**
@@ -225,9 +238,14 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
+function tallyUpDonations(runners) {
   /* CODE HERE */
+  let totalDonationsArray = [0]
+  runners.forEach(runner => totalDonationsArray.push(runner.donation));
+  let talliedNumber = totalDonationsArray.reduce((total, num) => total += num);
+  return talliedNumber;
 }
+
 
 /////////////// CLOSURES ///////////////
 /////////////// CLOSURES ///////////////
@@ -248,8 +266,9 @@ function tallyUpDonations(/* CODE HERE */) {
 function counterMaker() {
   // BROKEN CODE STARTS
   const count = 0;
-  function counter() {
+  return function counter() {
     ++count
+    return count - 1
   }
   // BROKEN CODE ENDS
 }
@@ -274,8 +293,19 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
+function counterMakerWithLimit(maxCount) {
   /* CODE HERE */
+  let count = -1;
+  return function counter(){
+    if (count<maxCount){
+      ++count
+      return count 
+    }
+    else (count = maxCount){
+      count = 0;
+      return count;
+}
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
